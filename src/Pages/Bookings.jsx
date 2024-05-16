cd7bc8f2c2d0495804c23345ad31a06d58e71cf8
Sa18-f <sisazid182713@gmail.com>
2024-05-16 19:55:44 +0600

    logout token deleted

--- src/Pages/Bookings.jsx
+++ src/Pages/Bookings.jsx
@@ -2,7 +2,6 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
 import BookingRow from "./BookingRow";
 import Swal from "sweetalert2";
-import axios from "axios";
 
 
 const Bookings = () => {
@@ -10,13 +9,13 @@ const Bookings = () => {
     const [bookings, setBookings] = useState([]);
     const url = `http://localhost:5000/bookings?email=${user.email}`;
     useEffect(() => {
-        axios.get(url, {withCredentials: true})
-        .then(res => {
-            setBookings(res.data);
-        })
-        // fetch(url)
-        //     .then(res => res.json())
-        //     .then(data => setBookings(data))
+        // axios.get(url, {withCredentials: true})
+        // .then(res => {
+        //     setBookings(res.data);
+        // })
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBookings(data))
     }, [url]);
     const handleDelete = id => {
         Swal.fire({
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
@@ -24,8 +25,23 @@ const AuthProviders = ({children}) => {
 
     useEffect(() => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser = {email: userEmail};
             setUser(currentUser);
-            setLoading(false)
+            setLoading(false);
+            // if user exists then issue an token
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log('token response', res.data)
+                })
+            }
+            else{
+                axios.post('http://localhost:5000/logout', loggedUser, {withCredentials: true})
+                .then(res => {
+                    console.log(res.data)
+                })
+            }
         })
         return(() => {
             return unSubscribe();
